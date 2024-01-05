eb52060c62c878031be6844ece879569c1e249b4
Yasmin Beatriz <yasmin@profusion.mobi>
2024-01-05 17:30:29 -0300

    feat: bootstrap nextjs-learn tutorial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import { Metadata } from 'next';
 
-import './globals.css';
+import '@/app/ui/global.css';
 
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import { inter } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: `TypeScript starter for Next.js`,
   description: `TypeScript starter for Next.js that includes all you need to build amazing apps`,
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
